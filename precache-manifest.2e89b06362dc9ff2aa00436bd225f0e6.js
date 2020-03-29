f2645d21bb9c11a3e31c4fb88581d0e88df06106
Michel Böker <michel-boeker@gmx.de>
2020-03-29 21:51:50 +0200

    Updates

---
+++ precache-manifest.2e89b06362dc9ff2aa00436bd225f0e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac70dfe337f9f366534cda5823a60ff9",
+    "url": "/star-wars-app/index.html"
+  },
+  {
+    "revision": "e648ceb2e123991bb472",
+    "url": "/star-wars-app/static/css/main.988fed9a.chunk.css"
+  },
+  {
+    "revision": "301d2e4888d55f267707",
+    "url": "/star-wars-app/static/js/2.bc8e84e4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/star-wars-app/static/js/2.bc8e84e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e648ceb2e123991bb472",
+    "url": "/star-wars-app/static/js/main.e7d14f68.chunk.js"
+  },
+  {
+    "revision": "ee9eeab08202084e5425",
+    "url": "/star-wars-app/static/js/runtime-main.b5991d84.js"
+  },
+  {
+    "revision": "53fcb7757b186cc1b203ff7df36b1619",
+    "url": "/star-wars-app/static/media/stars.53fcb775.jpg"
+  }
+]);
